cdfeba4ebfbdd7965989590c264e09d977cb1141
Artem <cartem33@gmail.com>
2025-02-16 14:21:21 +0300

    3.3. Фиксация изменений на github [#243564 #394629]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Artem Chigarov");
+        String birthday = "22.12.2002";
+        System.out.println(birthday);
+    }
+
+}
