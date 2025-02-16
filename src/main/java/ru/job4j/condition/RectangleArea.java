1d9107c02760309643e904dbad34d1b64029a5fe
Artem <cartem33@gmail.com>
2025-02-16 19:34:57 +0300

    8. Стороны прямоугольника. [#137735 #396612]

---
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class RectangleArea {
+
+    public static double square(double p, double k) {
+        double result = -1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+    }
+}
