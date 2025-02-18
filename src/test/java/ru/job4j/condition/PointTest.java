6e6bd304d0701333cfc7746e09505ad11651007b
Artem <cartem33@gmail.com>
2025-02-18 17:19:37 +0300

    4. Тесты для стороны прямоугольника. [#159460 #397419]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class PointTest {
