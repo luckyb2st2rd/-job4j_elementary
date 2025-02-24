afbc1f70cd3a99da141b0ce55fd6181e14fafeec
Artem <cartem33@gmail.com>
2025-02-24 17:46:37 +0300

    4.5. Отладка программы в IDEA [#159464 #398048]

--- src/test/java/ru/job4j/condition/RectangleAreaTest.java
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -15,6 +15,15 @@ class RectangleAreaTest {
     }
 
     @Test
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        double k = 1;
+        double out = RectangleArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
+    }
+
+    @Test
     void whenP20K8Square9Dot87() {
         int p = 20;
         double k = 8;
