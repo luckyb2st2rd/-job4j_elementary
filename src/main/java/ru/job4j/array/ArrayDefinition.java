42a582161b2285539767f033bda01dadce6e82a9
Artem <cartem33@gmail.com>
2025-04-22 13:41:53 +0300

    6.0.3. Заполнение массива. [#226300 #402091]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,21 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        numbers[0] = 2006;
+        numbers[1] = 2010;
+        numbers[2] = 2014;
+        numbers[3] = 2018;
+        numbers[4] = 2022;
+        int years = numbers[3];
+        System.out.println(years);
+        System.out.println(numbers[3]);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Artem Chigarov";
+        names[2] = "Nurlan Safarov";
+        names[3] = "Sergey Kashirov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
